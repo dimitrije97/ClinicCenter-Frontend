898815b0fdd8c33ab4ddafa79e95549d46ddb9e7
laramimica <lara.mimica@gmail.com>
2020-04-10 21:02:33 +0200

    Izvestaj o posovanju klinike koji administrator klinike moze da vidi

---
+++ src/app/services/income.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IncomeService {
+
+  private baseUrl = environment.baseUrl;
+
+  constructor(private http: HttpClient) { }
+
+
+  public getClinicsIncome(id): Observable<any> {
+    return this.http.get(`${this.baseUrl}incomes/${id}/clinic`);
+  }
+}
