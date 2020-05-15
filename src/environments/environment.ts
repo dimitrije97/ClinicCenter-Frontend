22d5b43ea48ca64e0caa8b81f24139d96a2ff7d8
dimitrije97 <dimitrijesalic@yahoo.com>
2020-05-15 14:41:25 +0200

    url changed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost:8090/'
+  baseUrl: 'https://isa-b.herokuapp.com/'
 };
-
+//http://localhost:8090/ kad radis lokalno
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
